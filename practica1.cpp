d1f9a79b24e980bbaa91aed9e2dcfff931dc5211
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:14:59 -0500

    first commit

---
+++ practica1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c=0;
+
+    cout<< "ingresa un numero: " ;
+    cin >> a;
+    cout<< "\ningresa un numero: ";
+    cin >> b;
+
+    c=a+b;
+
+    cout << "\nla suma es: " << c;
+
+    getch();
+    return 0;
+}
